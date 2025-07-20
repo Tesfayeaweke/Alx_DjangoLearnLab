1e0e8425cefd097af22eb5a4e06d4398d6f96a24
Tesfaye Eshetu <tesfayee57@gmail.com>
2025-07-20 11:11:25 +0300

    second correction made.

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -12,7 +12,7 @@ class Book(models.Model):
     def __str__(self):
         return self.title
     
-class Library(models.Model):
+class library(models.Model):
     name  = models.CharField(100)
     books = models.ManyToManyField(Book)
     def __str__(self):
