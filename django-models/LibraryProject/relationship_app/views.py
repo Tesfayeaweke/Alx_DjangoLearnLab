28413daec1053ddf16969665acc535cbf376d4fd
Tesfaye Eshetu <tesfayee57@gmail.com>
2025-07-20 17:51:55 +0300

    Implemented user authentication.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from .models import Library,Book,Author,Librarian
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import logout as auth_logout
+
 def list_books(request):
     books = Book.objects.all()
     context = {'book_list': books}
@@ -19,4 +23,18 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['books'] = self.object.books.all()  # Access related books
         return context
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, f'Account created successfully for {user.username}! You can now log in.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Registration failed. Please correct the errors below.')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'relationship_app/register.html', {'form': form})
 
